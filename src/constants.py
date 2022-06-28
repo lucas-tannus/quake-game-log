b9e26086c9e6032da1f15c9ffd20187f2154c654
Lucas Tannus <lucastannus@lucastannus1.mac.hpicorp.net>
2022-06-28 14:49:25 -0300

    fix: adapting to create multiple games results

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,9 @@
 
 KILL_LINE_PATTERN = r"^\d+:\d+\sKill:\s(\d+( \d+)+):(\s+([<>_a-zA-Z]+\s+)+)"
+START_GAME_PATTERN = r"^\d+:\d+\sInitGame:.+"
 WORLD_PLAYER = "<world>"
+
+
+class LogConstants:
+    INFO = "info"
+    WARN = "warn"
--- src/main.py
+++ src/main.py
@@ -3,23 +3,16 @@ import os
 import re
 import sys
 
+from handlers import handle_results
 from constants import (
     KILL_LINE_PATTERN,
+    START_GAME_PATTERN,
     WORLD_PLAYER
 )
 
-GAME = {
-    "total_kills": 0,
-    "players": [],
-    "kills": {},
-    "kills_by_means": {}
-}
-
-
-def get_all_kill_lines(file_name):
-    regex = re.compile(KILL_LINE_PATTERN)
-    with open(os.path.join("data", file_name), "r") as f:
-        return [line for line in f.readlines() if regex.match(line.strip())]
+GAMES = {}
+CURRENT_GAME_KEY = ""
+NUMBER_OF_GAMES = 0
 
 
 def get_death_data(kill_line):
@@ -30,47 +23,84 @@ def get_death_data(kill_line):
 
 
 def increase_total_kills():
-    GAME["total_kills"] += 1
+    GAMES[CURRENT_GAME_KEY]["total_kills"] += 1
 
 
 def add_players(players):
     for player in players:
-        if player != WORLD_PLAYER and player not in GAME["players"]:
-            GAME["players"].append(player)
+        if player != WORLD_PLAYER and player not in GAMES[CURRENT_GAME_KEY]["players"]:
+            GAMES[CURRENT_GAME_KEY]["players"].append(player)
 
 
 def set_kill(killer, victim):
+    game = GAMES[CURRENT_GAME_KEY]
     if killer == WORLD_PLAYER:
-        GAME["kills"][victim] = -1 if victim not in GAME["kills"] else GAME["kills"][victim] - 1
+        game["kills"][victim] = -1 if victim not in game["kills"] else game["kills"][victim] - 1
     else:
-        GAME["kills"][killer] = 1 if killer not in GAME["kills"] else GAME["kills"][killer] + 1
+        game["kills"][killer] = 1 if killer not in game["kills"] else game["kills"][killer] + 1
 
 
-def set_mean(mean):
-    if mean in GAME["kills_by_means"]:
-        GAME["kills_by_means"][mean] += 1
+def set_meaning(meaning):
+    game = GAMES[CURRENT_GAME_KEY]
+
+    if meaning in game["kills_by_means"]:
+        game["kills_by_means"][meaning] += 1
     else:
-        GAME["kills_by_means"][mean] = 1
+        game["kills_by_means"][meaning] = 1
+
+
+def is_new_game(line):
+    return bool(re.compile(START_GAME_PATTERN).match(line.strip()))
+
+
+def is_kill_event(line):
+    return bool(re.compile(KILL_LINE_PATTERN).match(line.strip()))
+
+
+def create_game():
+    global CURRENT_GAME_KEY
+    CURRENT_GAME_KEY = f"game_{NUMBER_OF_GAMES}"
+    GAMES[CURRENT_GAME_KEY] = {
+        "total_kills": 0,
+        "players": [],
+        "kills": {},
+        "kills_by_means": {}
+    }
+
+
+def increase_number_of_games():
+    global NUMBER_OF_GAMES
+    NUMBER_OF_GAMES += 1
+
+
+@handle_results
+def create_games_grouped_information(file_name):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", file_name), "r") as f:
+        for line in f.readlines():
+            if is_new_game(line):
+                increase_number_of_games()
+                create_game()
+                continue
+
+            if is_kill_event(line):
+                increase_total_kills()
+                killer, victim, meaning = get_death_data(line)
+                add_players([killer, victim])
+                set_kill(killer, victim)
+                set_meaning(meaning)
+
+
+def show_result():
+    print(GAMES)
 
 
 def save_result():
-    with open(os.path.join("data", "result.txt"), "w") as f:
-        f.write(json.dumps(GAME, indent=4))
-
-
-def generate_group_information(file_name):
-    kill_lines = get_all_kill_lines(file_name)
-
-    for kill_line in kill_lines:
-        increase_total_kills()
-        killer, victim, mean = get_death_data(kill_line)
-        add_players([killer, victim])
-        set_kill(killer, victim)
-        set_mean(mean)
-
-    save_result()
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "result.txt"), "w") as f:
+        f.write(json.dumps(GAMES, indent=4))
 
 
 if __name__ == "__main__":
     file = sys.argv[-1]
-    generate_group_information(file)
+    create_games_grouped_information(file)
+    show_result()
+    save_result()
